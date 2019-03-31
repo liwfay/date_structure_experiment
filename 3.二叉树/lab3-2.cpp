3a6c2eeb060ec90aa56397adfd229499d7db58b9
落枫 <35682548+lofon@users.noreply.github.com>
2019-03-31 10:31:35 +0800

    Add files via upload

---
+++ 3.二叉树/lab3-2.cpp
@@ -0,0 +1,9 @@
+//lab3-2.cpp
+////////////////////////////////written by lofone at 2018/11/08/////////////////////////
+
+#include<stdio.h>
+#include<malloc.h>
+
+#define OK 1
+#define ERROR 0
+typedef char 
